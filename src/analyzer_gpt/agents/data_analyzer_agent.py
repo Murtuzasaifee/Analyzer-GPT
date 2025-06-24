6911180246027f62160547cea606f7427e36d63b
Murtuza Saifee <msaifee@firstorion.com>
2025-06-24 15:11:14 +0400

    Data analyzer prompt

--- src/analyzer_gpt/agents/data_analyzer_agent.py
+++ src/analyzer_gpt/agents/data_analyzer_agent.py
@@ -3,11 +3,11 @@ from src.analyzer_gpt.prompts.data_analyzer_prompt import DATA_ANAYLZER_PROMPT
 
 def getDataAnalyzerAgent(model_client):
     
-    problem_solver_expert = AssistantAgent(
+    data_analyzer_agent = AssistantAgent(
         name='DataAnalyzerAgent',
         description="An expert agent that analyzes data using code execution.",
         model_client=model_client,
         system_message=DATA_ANAYLZER_PROMPT
     )
 
-    return problem_solver_expert
+    return data_analyzer_agent
